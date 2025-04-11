9794c918e89a4124ff3a33ce799fc9f00d415014
simsonkamala <simsonkamala.abi@gmail.com>
2025-04-11 20:51:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 // mongodb://kirenadmin:kirenadmin@localhost:27017/"kireninvoice"?authSource=admin
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+mongoose.connect(process.env.MONGO_URL)
 const db = mongoose.connection
 
 // --------- For DataBase Connection -----------
